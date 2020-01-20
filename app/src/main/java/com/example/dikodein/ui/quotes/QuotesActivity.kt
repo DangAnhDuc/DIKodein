e30f93aaeca76fbb14a237fc08a68069f5d9ff61
Dang Anh Duc <anhduc.dang@my.jcu.edu.au>
2020-01-20 11:42:55 +0700

    Finialize

---
+++ app/src/main/java/com/example/dikodein/ui/quotes/QuotesActivity.kt
@@ -0,0 +1,47 @@
+package com.example.dikodein.ui.quotes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+import com.example.dikodein.R
+import com.example.dikodein.data.model.Quote
+import kotlinx.android.synthetic.main.activity_quotes.*
+import org.kodein.di.Kodein
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.closestKodein
+import org.kodein.di.generic.instance
+import java.lang.StringBuilder
+import kotlin.text.Typography.quote
+
+class QuotesActivity : AppCompatActivity(),KodeinAware {
+    override val kodein by  closestKodein()
+    private val  viewModelFactory: QuotesViewModelFactory by instance()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quotes)
+        initializeUI()
+    }
+
+    private fun initializeUI() {
+        val viewModel = ViewModelProviders.of(this,viewModelFactory).get(QuoteViewModel::class.java)
+
+        viewModel.getQuotes().observe(this, Observer { quotes ->
+            val stringBuilder = StringBuilder()
+            quotes.forEach { quote ->
+                stringBuilder.append("$quote\n\n")
+            }
+            textView_quotes.text= stringBuilder.toString()
+        })
+
+        button_add_quote.setOnClickListener(){
+            val quote = Quote(editText_quote.text.toString(),editText_author.text.toString())
+            viewModel.addQuote(quote)
+            editText_quote.setText("")
+            editText_author.setText("")
+        }
+    }
+
+
+}
